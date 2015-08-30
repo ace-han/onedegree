9057fd541ef4eb8ee730458b11833e1eeb34a822
Ace <ace.jl.han@gmail.com>
2015-08-31 00:23:13 +0800

    append slash in treetag ng-admin api via entity url function and refine tabular view's functionality

--- admin/static/admin/tag/constants/treeTagEntity.initializer.js
+++ admin/static/admin/tag/constants/treeTagEntity.initializer.js
@@ -15,32 +15,40 @@ define([
     	var entityName = 'tree-tags'
     	var treeTag = nga.entity(entityName)
 				        .baseApiUrl(moduleBaseApiUrl)
-						.label('Tree Tags (Tabular View)');
+						.label('Tree Tags (Tabular View)')
+	    		        .url(function(entityName, viewType, entityId){
+	    		        	// add a suffix to this to avoid new error
+	    		        	// refer to https://github.com/marmelab/ng-admin/issues/392?_pjax=%23js-repo-pjax-container
+	    		        	// but above link's arguments are outdated, plz debug it with sourcemap to see the function signature
+	    		        	// baseApiUrl could be retrieved via this._baseApiUrl and it's already '/' suffixed
+	    		        	var url = entityName + '/';
+	    		        	if(entityId){
+	    		        		url += entityId;
+	    		        	}
+	    		        	return url;
+	    		        });
     	
     	treeTag.listView()
 	        .title('All tree tags') // default title is "[Entity_name] list"
 	        .description('List of tree tags with infinite pagination') // description appears under the title
 	        .infinitePagination(false) // (deprecated)load pages as the user scrolls. using pagination to reduce page load
 	        .fields([
-	            nga.field('id').label('ID'), // The default displayed name is the camelCase field name. label() overrides id
-	            nga.field('name'), // the default list field type is "string", and displays as a string
-	            nga.field('slug'),
-	            nga.field('parent_id', 'reference')
+	            nga.field('id').label('ID') // The default displayed name is the camelCase field name. label() overrides id
+	            , nga.field('name') // the default list field type is "string", and displays as a string
+	            , nga.field('slug')
+	            , nga.field('parent', 'reference')
 	            	.label('Parent')
 	            	.targetEntity(treeTag)
 	            	.targetField(nga.field('name')),
-	            nga.field('tree_id'),
-	            nga.field('level')
+	            , nga.field('tree_id'),
+	            , nga.field('level')
 	        ])
 	        .filters([
                 nga.field('q', 'template')
                     .label('')
                     .pinned(true)
                     .template('<div class="input-group"><input type="text" ng-model="value" placeholder="Search" class="form-control"></input><span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span></div>'),
-                nga.field('tree_id')
-                    .label('Tree ID')
-                    .attributes({'placeholder': 'Filter by tree id'}),
-                nga.field('parent_id', 'reference')
+                nga.field('parent', 'reference')
                     .label('Parent')
                     .targetEntity(treeTag)
                     .targetField(nga.field('name'))
@@ -51,21 +59,24 @@ define([
     	
     	treeTag.creationView()
 	        .fields([
-	            nga.field('id').label('ID'), // The default displayed name is the camelCase field name. label() overrides id
-	            nga.field('name'), // the default list field type is "string", and displays as a string
-	            nga.field('slug'),
-	            nga.field('parent_id', 'reference')
+	            nga.field('name')					// the default list field type is "string", and displays as a string
+	            	.validation({ required: true }) 
+	            , nga.field('slug')
+	            , nga.field('parent', 'reference')
 	            	.label('Parent')
 	            	.targetEntity(treeTag)
 	            	.targetField(nga.field('name'))
 	            	.validation({ required: true })
-	            	.remoteComplete(true, { refreshDelay: 0 }),
-	            nga.field('tree_id'),
-	            nga.field('level')
+	            	.remoteComplete(true, { refreshDelay: 0 })
 	        ]);
     	
     	treeTag.editionView()
-        	.fields(treeTag.creationView().fields())
+        	.fields([
+					nga.field('name')					// the default list field type is "string", and displays as a string
+						.validation({ required: true })
+					, nga.field('slug')
+						.validation({ required: true })
+        	        ]);
     	
     	
     	moduleMenu
