c64ab3bb9d48b8943f866ac506ee823abf879b95
Ace <ace.jl.han@gmail.com>
2015-10-19 00:45:42 +0800

    add some common permission on authx module

---
+++ authx/permissions.py
@@ -0,0 +1,29 @@
+from rest_framework import permissions
+from rest_framework.permissions import BasePermission
+
+
+class IsSuperUser(BasePermission):
+    """
+    Allows access only to admin users.
+    """
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_superuser
+    
+class IsOwnerOrReadOnly(BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        owner = obj.owner if hasattr(obj, 'owner') else obj.user
+        if owner:
+            return owner.id == request.user.id
+        else:
+            return False
