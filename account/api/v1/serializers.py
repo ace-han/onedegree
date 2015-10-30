66f7dd769585491db6bc1310e9483fbb4d06646b
Ace <ace.jl.han@gmail.com>
2015-10-31 02:00:44 +0800

    make `patch` method available to user profile serialiser

--- account/api/v1/serializers.py
+++ account/api/v1/serializers.py
@@ -12,9 +12,15 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'nickname', 'selfie_path', 'email')
         
 class SchoolSerializer(serializers.ModelSerializer):
+    #id = serializers.AutoF
     class Meta:
         model = School
         fields = ('id', 'name')
+        # this read_only is for nested Serializer getting id as a writable field
+        # see/debug to below src code for more detail
+        #  rest_framework.serializers.py#to_internal_value
+        #  rest_framework.serializers.py#_writable_fields(self):
+        extra_kwargs = {'id': {'read_only': False,}, } 
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
@@ -29,3 +35,13 @@ class UserProfileSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True)
     class Meta:
         model = Profile
+        
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            if attr in ('high_school', 'college'):
+                # a little hack here for the value(school) is {'id':0,'name':'x',}
+                value = School.objects.get(id=value.get('id'))
+            setattr(instance, attr, value)
+        instance.save()
+
+        return instance
